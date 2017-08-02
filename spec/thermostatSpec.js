e86c3196492dca7dd3194b0f9b63ac212f1bca97
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-02 14:36:23 +0100

    implemented temperature increasing and decreasing

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,6 +2,21 @@ describe('Thermostat', function() {
   var thermostat = new Thermostat;
 
   it('has a temp', function() {
-    expect(thermostat.temp).toEqual(20)
+    expect(thermostat.temp).toEqual(20);
+  });
+
+  describe('tempIncrease', function() {
+    it('can have its temperature increased', function() {
+      thermostat.tempIncrease();
+      expect(thermostat.temp).toEqual(21);
+    });
+  });
+
+  describe('tempDecrease', function() {
+    it('can have its temperature decreased', function() {
+      var thermostat2 = new Thermostat;
+      thermostat2.tempDecrease();
+      expect(thermostat2.temp).toEqual(19);
+    });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temp = 20;
 }
+
+Thermostat.prototype.tempIncrease = function () {
+  this.temp++
+};
+
+Thermostat.prototype.tempDecrease = function () {
+  this.temp--
+};
